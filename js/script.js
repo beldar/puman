d24886951ec8c91a1c1e154666662f8a9f01c6ca
Beldar <beldar.cat@gmail.com>
2018-08-09 21:01:16 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#carousel').owlCarousel({
+    autoPlay: 9000,
+    navigation : true,
+    navigationText: ['Anterior', 'Següent'],
+    slideSpeed : 900,
+    singleItem : true,
+    pagination : true
+  });
+});
